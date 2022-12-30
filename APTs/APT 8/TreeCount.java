1878690251c69a135b6832a8103f72e4eacb32f8
Dav King <david.king@duke.edu>
2022-12-30 15:05:18 -0500

    Update TreeCount.java

--- APTs/APT 8/TreeCount.java
+++ APTs/APT 8/TreeCount.java
@@ -1,3 +1,5 @@
+/*Write a method that returns the number of nodes of a binary tree. The TreeNode class will be accessible when your method is tested. */
+
 public class TreeCount {
     int sum = 0;
     public int count(TreeNode tree) {

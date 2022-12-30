6c9eeaccda2eb62d07cd96cd0189597c0e2a0d8b
Dav King <david.king@duke.edu>
2022-12-30 15:02:50 -0500

    Update HeightLabel.java

--- APTs/APT 8/HeightLabel.java
+++ APTs/APT 8/HeightLabel.java
@@ -1,3 +1,11 @@
+/*Write method rewire that returns a new tree, isomorphic (same shape) as the tree parameter,
+but in which each node's info field is equal to that node's height in the tree.
+
+Given the tree diagrammed below on the left, the tree returned should be as diagrammed on the right.
+The values in the tree passed as a parameter don't matter. You'll write code to create a new tree in
+which the value in each node is that node's height in the tree returned. Recall that height is measured
+in nodes, and it's the length of the longest root-to-leaf path in a tree. */
+
 public class HeightLabel {
     //public int num = 0;
     public TreeNode rewire(TreeNode t) {

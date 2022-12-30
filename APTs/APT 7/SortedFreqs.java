8bd4607344c42ecefa3388e9507971d4ad971a40
Dav King <david.king@duke.edu>
2022-12-30 15:02:02 -0500

    Update SortedFreqs.java

--- APTs/APT 7/SortedFreqs.java
+++ APTs/APT 7/SortedFreqs.java
@@ -1,3 +1,18 @@
+/*The frequency with which data occurs is sometimes an important statistic. In this problem
+you'll determine how frequently strings occur and return an array representing the frequencies
+of each different/unique string. The array returned contains as many frequencies as there are
+unique strings. The returned frequencies represent an alphabetic/lexicographic ordering of the
+unique words, so the first frequency is how many times the alphabetically first word occurs and
+the last frequency is the number of times the alphabetically last word occurs.
+
+Consider these strings (quotes for clarity, they're not part of the strings).
+
+
+{"apple", "pear", "cherry", "apple", "cherry", "pear", "apple", "banana"}
+
+The array returned is {3,1,2,2} since the alphabetically first word is "apple" which occurs 3 times;
+the second word alphabetically is "banana" which occurs once, and the other words each occur twice. */
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.TreeSet;

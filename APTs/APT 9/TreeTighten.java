7ddf0c02820ad0a291def2067ef9e72c892263e4
Dav King <david.king@duke.edu>
2022-12-30 15:07:17 -0500

    Update TreeTighten.java

--- APTs/APT 9/TreeTighten.java
+++ APTs/APT 9/TreeTighten.java
@@ -1,3 +1,12 @@
+/*Write method tighten that removes every node with a single child (either left or right),
+replacing such nodes with their single child. This tightening process is repeatedly applied
+so that when completed all nodes have either zero or two children.
+
+In the tree diagrammed below, the call tighten(t) applied to the tree on the left modifies and
+returns the tree shown on the right. Note that the nodes with one child on the left: 22, 18, and 15
+are each removed and replaced by their single child. For 22 this means that 13 left's child is
+8 and 13's right child is 25 since both 18 and 15 are removed. */
+
 public class TreeTighten {
         public TreeNode tighten(TreeNode t) {
             if(t == null || (t.left == null && t.right == null)){

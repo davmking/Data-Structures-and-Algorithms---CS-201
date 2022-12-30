b7fc094ce11e1554136706a22649ce4a1a724442
Dav King <david.king@duke.edu>
2022-12-30 15:06:28 -0500

    Update LeafTrails.java

--- APTs/APT 9/LeafTrails.java
+++ APTs/APT 9/LeafTrails.java
@@ -1,3 +1,12 @@
+/*Write method trails that returns an array of the binary encoded root-to-leaf paths for each leaf node of its tree parameter tree.
+
+Values stored in leaf nodes will be unique, that is the same value will not appear in more than one leaf.
+
+In traversing from the root to a leaf a path is represented by going left or right. Going left is encoding
+by a zero and going right is encoded by a one. The path left,left,right would thus be encoded as "001" in the binary encoding of the root-to-leaf path.
+
+The strings stored in the array should be sorted by the value stored in the leaf node represented by the path. See the examples for details. */
+
 import java.util.TreeMap;
 
 public class LeafTrails {

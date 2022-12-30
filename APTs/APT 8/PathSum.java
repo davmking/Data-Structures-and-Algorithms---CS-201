eccf0ceeea1880fe70fd7e34e2ae409e1cd602ad
Dav King <david.king@duke.edu>
2022-12-30 15:04:10 -0500

    Update PathSum.java

--- APTs/APT 8/PathSum.java
+++ APTs/APT 8/PathSum.java
@@ -1,3 +1,10 @@
+/*Write a method that returns 1 if there is a root-to-leaf path whose node values sum
+to target and returns 0 if there is no such root-to-leaf path.
+
+For example, in the tree below there are exactly four root-to-leaf paths. The sums on
+these paths are 27, 22, 26, 18, so hasPathSum(22,tree) will return 1 for the tree shown
+and hasPathSum(32,tree) will return 0 for the tree shown. */
+
 public class PathSum {
     public int hasPath(int target, TreeNode tree){
         if(tree == null){

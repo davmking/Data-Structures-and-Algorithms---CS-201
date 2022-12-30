dc48fe25a9ece7d722cb0f656b31d2e4ae29c143
Dav King <david.king@duke.edu>
2022-12-30 15:09:43 -0500

    Update VoteRigging.java

--- APTs/APT 10/VoteRigging.java
+++ APTs/APT 10/VoteRigging.java
@@ -1,3 +1,9 @@
+/*You have used your secret mind-reading device to find out how every voter will vote in the next election.
+Your mind-reading device also revealed to you that all the voters are prepared to change their vote if you pay them enough.
+
+The ith element of votes is the number of people who were originally planning to vote for candidate i.
+Return the minimum number of votes that you must change to ensure that candidate 0 (your favorite) will have more votes than any other candidate. */
+
 public class VoteRigging {
     public int minimumVotes(int[] votes) {
         int max = findMax(votes);
